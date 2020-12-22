fca2969110b2c355ef1e15fc39c754557c2d4e66
josh1506 <jmjabor@gmail.com>
2020-12-22 16:00:33 +0800

    Adding Carousel

--- src/components/common/carouselFunc.jsx
+++ src/components/common/carouselFunc.jsx
@@ -2,44 +2,38 @@ import React, { useState, useEffect } from 'react';
 import { Carousel } from 'react-bootstrap'
 
 function CarouselFunc({ carouselImages }) {
-    const [carouselList, setCarouselList] = useState([])
-
-    useEffect(() => setCarouselList(carouselImages))
-
+    const carousel = [
+        {
+            src: "https://www.natureindex.com/news-blog/image/5e2f8a77d3ff127d0d116851",
+            alt: "First slide",
+            title: "First slide label",
+            detail: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+        },
+        {
+            src: "https://www.natureindex.com/news-blog/image/5e2f8a77d3ff127d0d116851",
+            alt: "First slide",
+            title: "First slide label",
+            detail: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+        },
+        {
+            src: "https://www.natureindex.com/news-blog/image/5e2f8a77d3ff127d0d116851",
+            alt: "First slide",
+            title: "First slide label",
+            detail: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+        },
+    ]
     return (<Carousel>
-        <Carousel.Item interval={1000}>
+        {carousel.map(item => <Carousel.Item interval={1000}>
             <img
                 className="d-block w-100"
-                src="https://www.natureindex.com/news-blog/image/5e2f8a77d3ff127d0d116851"
-                alt="First slide"
+                src={item.src}
+                alt={item.alt}
             />
             <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                <h3>{item.title}</h3>
+                <p>{item.detail}</p>
             </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
-            <img
-                className="d-block w-100"
-                src="https://www.natureindex.com/news-blog/image/5e2f8a77d3ff127d0d116851"
-                alt="Third slide"
-            />
-            <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-            <img
-                className="d-block w-100"
-                src="https://www.natureindex.com/news-blog/image/5e2f8a77d3ff127d0d116851"
-                alt="Third slide"
-            />
-            <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
-        </Carousel.Item>
+        </Carousel.Item>)}
     </Carousel>
     );
 }
