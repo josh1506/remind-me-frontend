be4729a96957302ced95fae2c5ad5bcd44e2692a
josh1506 <jmjabor@gmail.com>
2020-12-26 22:54:43 +0800

    Adding FormClass for reusable component

--- src/components/common/formClass.jsx
+++ src/components/common/formClass.jsx
@@ -1,23 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Form, Button } from 'react-bootstrap'
 
-class FormClass extends Component {
-    renderInput = (name, label, type = 'text', ...rest) => {
-        return (
-            <React.Fragment>
-                <Form.Label>{label}</Form.Label>
-                <Form.Control type={type} name={name} placeholder={label} {...rest} />
-            </React.Fragment>
-        )
-    }
-
-    renderText = (text) => <Form.Text className="text-muted"> {text}</Form.Text >
-
-    renderButton = (label, variant, ifBlock = false) => <Button variant={variant} type="submit" block={ifBlock} >{label}</Button>
-
-    renderCheckBox = (type, label) => <Form.Check type={type} label={label} />
 
 
+
+export const renderInput = (name, label, type = 'text', ...rest) => {
+    return (
+        <React.Fragment>
+            <Form.Label>{label}</Form.Label>
+            <Form.Control type={type} name={name} placeholder={label} {...rest} />
+        </React.Fragment>
+    )
 }
 
-export default FormClass;
+export const renderText = (text) => <Form.Text className="text-muted"> {text}</Form.Text >
+
+export const renderButton = (label, variant, ifBlock = false) => <Button variant={variant} type="submit" block={ifBlock}>{label}</Button>
+
+export const renderCheckBox = (type, label) => <Form.Check type={type} label={label} />
+
+function formClass(props) {
+}
+
+export default formClass;
