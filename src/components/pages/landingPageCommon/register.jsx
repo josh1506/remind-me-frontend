f73967fc55783c416206bacd9debb3dd79e6a936
josh1506 <jmjabor@gmail.com>
2020-12-28 22:09:41 +0800

    Adding register form in Landing Page

--- src/components/pages/landingPageCommon/register.jsx
+++ src/components/pages/landingPageCommon/register.jsx
@@ -1,49 +1,65 @@
-import React from 'react';
-import { Row, Col, Form } from 'react-bootstrap'
-import FormClass from './../../common/formClass';
+import React, { useState } from 'react';
+import { Row, Col, Form, Button } from 'react-bootstrap'
+import axios from 'axios'
 
 
-class Register extends FormClass {
-    render() {
-        return (
-            <React.Fragment>
-                <Row className='justify-content-md-center mt-5'>
-                    <h1>Register Account</h1>
-                    <Form style={{ width: '75%' }}>
+function Register(props) {
+    const [user, setUser] = useState({ username: '', email: '', password: '' })
+    const [] = useState({ verifiedPassword: ''})
 
-                        <Form.Group controlId="formBasicUsername">
-                            {this.renderInput('username', 'Username')}
-                            {this.renderText('Username will be used to display account name')}
-                        </Form.Group>
-
-                        <Form.Group controlId="formBasicEmail">
-                            {this.renderInput('email', 'Email', 'email')}
-                            {this.renderText("We'll never share your email with anyone else.")}
-                        </Form.Group>
-
-                        <Form.Group controlId="formBasicPassword">
-                            {this.renderInput('password', 'Password', 'password')}
-                        </Form.Group>
-
-                        <Form.Group controlId="formBasicPassword">
-                            {this.renderInput('password', 'Confirm Password', 'password')}
-                            {this.renderText('Re-enter your password')}
-                        </Form.Group>
-
-                        <Row className='mt-5'>
-                            <Col xs lg="9" fluid>
-                                <a href="">Cancel</a>
-                            </Col>
-                            <Col className='ml-2'>
-                                {this.renderButton('Register', 'primary')}
-                            </Col>
-                        </Row>
-                    </Form>
-                </Row>
-            </React.Fragment>
-        );
+    const validateButton = () => {
+        if (user.password !== user.verifiedPassword) return true
+        if (user.password === '') return true
+        return false
     }
-}
 
+    const handleRegister = async (event) => {
+        event.preventDefault()
+
+        const { data } = await axios.post('https://remindme-backend.herokuapp.com/auth/register/', user)
+        console.log(data);
+    }
+    return (
+        <React.Fragment>
+            <Row className='justify-content-md-center mt-5'>
+                <h1>Register Account</h1>
+                <Form style={{ width: '75%' }}>
+
+                    <Form.Group controlId="formBasicUsername">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control value={user.username} onChange={event => setUser({ ...user, username: event.target.value })} type='text' name='username' placeholder='Username'></Form.Control>
+                        <Form.Text className="text-muted">Username will be used to display account name</Form.Text >
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control value={user.email} onChange={event => setUser({ ...user, email: event.target.value })} type='email' name='email' placeholder='Email'></Form.Control>
+                        <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text >
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control value={user.password} onChange={event => setUser({ ...user, password: event.target.value })} type='password' name='password' placeholder='Password'></Form.Control>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control value={user.verifiedPassword} onChange={event => setUser({ ...user, verifiedPassword: event.target.value })} type='password' name='verifiedPassword' placeholder='Confirm Password'></Form.Control>
+                        <Form.Text className="text-muted">Re-enter your password</Form.Text >
+                    </Form.Group>
+
+                    <Row className='mt-5'>
+                        <Col xs lg="9" fluid>
+                            <a href="" onClick={() => setUser({ username: '', email: '', password: '', verifiedPassword: '' })}>Cancel</a>
+                        </Col>
+                        <Col className='ml-2'>
+                            <Button variant='primary' type="submit" disabled={validateButton()} onClick={event => handleRegister(event)}>Register</Button>
+                        </Col>
+                    </Row>
+                </Form>
+            </Row>
+        </React.Fragment>
+    );
+}
 
 export default Register;
