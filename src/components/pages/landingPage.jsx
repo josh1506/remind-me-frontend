f04eb3891764177bbc1df871744c5f8068d068a1
josh1506 <jmjabor@gmail.com>
2021-01-03 11:35:32 +0800

    Adding landing page

--- src/components/pages/landingPage.jsx
+++ src/components/pages/landingPage.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Carousel } from 'react-bootstrap'
 import Login from './landingPageCommon/login';
 import Register from './landingPageCommon/register';
 import ForgotPass from './landingPageCommon/forgotpass';
 import CarouselFunc from './../common/carouselFunc';
+import axios from 'axios'
 
 
 function LandingPage(props) {
@@ -14,8 +15,8 @@ function LandingPage(props) {
                     <CarouselFunc />
                 </Col>
                 <Col xs lg="5">
-                    <Login />
-                    {/* <Register /> */}
+                    {/* <Login /> */}
+                    <Register />
                     {/* <ForgotPass /> */}
                 </Col>
 
