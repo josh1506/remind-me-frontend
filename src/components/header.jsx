a4fb8283be569d8b9864de88cffba5f289b876cb
josh1506 <jmjabor@gmail.com>
2021-01-26 15:52:15 +0800

    adding navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import NavBar from './nav';
 
 function Header(props) {
     return (
         <div>
+            <NavBar />
         </div>
     );
 }
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
-function NavBarComponent(props) {
+function NavBar(props) {
     return (
-        < React.Fragment >
-        </React.Fragment >
+        <nav>
+            <img src="" alt="logo" />
+            <span>
+                <span>
+                    <a href="#">Reminder</a>
+                    <select name="workspace" id="nav-workspace">
+                        <option value="1">Sample 1</option>
+                        <option value="2">Sample 2</option>
+                        <option value="3">Sample 3</option>
+                    </select>
+                    <a href="#">Reminder</a>
+                </span>
+                <span>
+                    <a href="#">Login</a>
+                    <a href="#">Logout</a>
+                </span>
+            </span>
+        </nav>
     );
 }
 
-export default NavBarComponent;
+export default NavBar;
