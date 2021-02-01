89fb2862cbcd895541c54fcdec786932bb5ae37c
josh1506 <jmjabor@gmail.com>
2021-02-01 21:37:03 +0800

    refactoring auth store by changing to slice

--- src/app/auth.js
+++ src/app/auth.js
@@ -1,34 +1,39 @@
-import { createAction, createReducer } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
-export const authCreated = createAction('authCreated')
-export const authRemoved = createAction('authRemoved')
 
-export default createReducer({
-            id: '',
-            username: '',
-            email: '',
-            tokens: {
-                refreshToken: '' ,
-                accessToken: ''
-            }
-        }, {
-    [authCreated.type]: (auth, action) => {
-        auth.id= action.payload.id
-        auth.username= action.payload.username
-        auth.email= action.payload.email
-        auth.tokens= {
-            refreshToken: action.payload.tokens.refreshToken,
-            accessToken: action.payload.tokens.accessToken
+const slice = createSlice({
+    name: 'auth',
+    initialState: {
+        id: '',
+        username: '',
+        email: '',
+        tokens: {
+            refreshToken: '' ,
+            accessToken: ''
         }
     },
-    [authRemoved.type]: (auth, action) => {
+    reducers: {
+        authCreated: (auth, action) => {
+            auth.id= action.payload.id
+            auth.username= action.payload.username
+            auth.email= action.payload.email
+            auth.tokens= {
+                refreshToken: action.payload.tokens.refreshToken,
+                accessToken: action.payload.tokens.accessToken
+            }
+        }, 
+        authRemoved: (auth, action) => {
         auth.id = ''
         auth.username = ''
         auth.email = ''
         auth.tokens = {
             refreshToken: '' ,
             accessToken: ''
+            }
         }
     }
-
 })
+
+
+export const {authCreated, authRemoved} = slice.actions
+export default slice.reducer
